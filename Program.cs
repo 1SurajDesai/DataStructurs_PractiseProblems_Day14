b7568a1d4961269d493c37d1b97960eefaede6d1
1SurajDesai <surajdesai219@gmail.com>
2022-03-09 22:12:29 +0530

    Adding program.cs file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace DataStructureProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Structure Problems");
+
+        }
+    }
+}
